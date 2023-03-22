4a2e9ce834064021a5d774299aa2d23f0e79b29d
thinh1310 <113665001+thinh1310@users.noreply.github.com>
2023-03-23 01:27:43 +0700

    Add project files.

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,56 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        
+        
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CouseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CouseViewModel viewModel)
+        {
+            
+                if (!ModelState.IsValid)
+                {
+                    viewModel.Categories = _dbContext.Categories.ToList();
+                    return View("Create", viewModel);
+                }
+                var course = new Course
+            {
+                lecturnerId = User.Identity.GetUserId(),
+                datetime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturet { get; set; }
+
+        public string lecturnerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string place { get; set; }
+        public DateTime datetime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
